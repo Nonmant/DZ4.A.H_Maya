9c690f90b986b4cbd2d2daa5592accd2e20dd0c4
Anton Bolotskih <89161424838@yandex.ru>
2021-11-25 00:17:15 +0300

    Probably looped on 13 test

--- funcs.cpp
+++ funcs.cpp
@@ -5,7 +5,7 @@
 
 void parseFile(std::istream & input, std::ostream & output){
 
-    std::unordered_multiset<char> word;
+    std::multiset<char> word;
     std::vector<char> sequence;
     size_t g, s;
     input >> g >> s;
@@ -16,14 +16,43 @@ void parseFile(std::istream & input, std::ostream & output){
     input.ignore();
     std::copy_n(std::istream_iterator<char>(input),s,sequence.begin());
 
-    std::unordered_multiset<char> curWord(sequence.begin(), sequence.begin()+(g-1));
+    std::multiset<char> curWord(sequence.begin(), sequence.begin()+(g-1));
     unsigned int count = 0;
-    for(int i = 0; i<=s-g; ++i){
+    for(unsigned int i = 0; i<=s-g; ++i){
+
+        if(word.count(sequence[i+g-1])==0){
+            i = i+g;
+            if(i > s-g)
+                break;
+            curWord = std::multiset<char>(sequence.begin() + i, sequence.begin() + (i + g - 1));
+            --i;
+            continue;
+        }
+
         curWord.insert(sequence[i+g-1]);
-        if(curWord == word)
-            count+=1;
+
+        std::vector<char> difference;
+        std::set_difference(word.begin(), word.end(),
+                            curWord.begin(), curWord.end(),
+                            std::back_inserter(difference));
 
         curWord.erase(curWord.find(sequence[i]));
+
+        if(difference.empty())
+            ++count;
+        else{
+            unsigned int skipSize = difference.size()-1;
+
+            if(skipSize+i >= s-g)
+                break;
+
+            for(unsigned int j = 1; j < skipSize; ++j){
+                curWord.erase(curWord.find(sequence[i+j]));
+                curWord.insert(sequence[i+j+g-1]);
+            }
+            if(skipSize > 0)
+                i+=skipSize-1;
+        }
     }
     output << count;
 }
